7f1f284dd19d72d689a09f0343026b47272bef91
Wanqi_Zhou <149352957+ElleZWQ@users.noreply.github.com>
2024-07-14 22:55:02 +0900

    Update __init__.py

--- tgc/data/__init__.py
+++ tgc/data/__init__.py
@@ -1,5 +1,7 @@
 from .lorenz import lorenz_96
 from .var import var_stable
+from .fMRI import fmri_net_sim
+from .dream3 import dream3_trajectories
+from .dream4 import dream4_trajectories
 
-
-__all__ = ['lorenz_96', 'var_stable']
+__all__ = ['lorenz_96', 'var_stable', 'fmri_net_sim', 'dream3_trajectories','dream4_trajectories']
